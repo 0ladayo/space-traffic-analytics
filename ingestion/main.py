f5290298758e0bcaf769fd6fdd01161ef1946ae7
0ladayo <Oladayo.Siyanbola@yahoo.com>
2026-01-03 23:43:20 +0000

    feat: add space debris data sources to ingestion pipeline
    - integrate 4 new data sources for tracking satellite debris
    - add 'TYPE' column to distinguish between 'Active' satellites and
      'Debris'

--- ingestion/main.py
+++ ingestion/main.py
@@ -10,7 +10,15 @@ logging.info('script started ...')
 
 @dlt.resource(table_name = 'orbital_satellites_data', write_disposition = 'replace', file_format = 'parquet')
 def load_satellites_data():
-    url = 'https://celestrak.com/NORAD/elements/gp.php?GROUP=active&FORMAT=csv'
+    urls = ['https://celestrak.com/NORAD/elements/gp.php?GROUP=active&FORMAT=csv', 
+    'https://celestrak.org/NORAD/elements/gp.php?GROUP=cosmos-1408-debris&FORMAT=csv',
+    'https://celestrak.org/NORAD/elements/gp.php?GROUP=fengyun-1c-debris&FORMAT=csv',
+    'https://celestrak.org/NORAD/elements/gp.php?GROUP=iridium-33-debris&FORMAT=csv',
+    'https://celestrak.org/NORAD/elements/gp.php?GROUP=cosmos-2251-debris&FORMAT=csv',
+    ]
+    
+    labels = ['Active', 'Debris (Cosmos 1408)', 'Debris (Fengyun 1C)', 'Debris (Iridium 33)', 'Debris (Cosmos 2251)']
+
     dtypes = {
         'MEAN_MOTION_DDOT': float, 
         'MEAN_MOTION_DOT': float,  
@@ -22,9 +30,12 @@ def load_satellites_data():
         'ARG_OF_PERICENTER': float,
         'MEAN_ANOMALY': float,
     }
-    logging.info(f'fetching data from {url}...')
-    for chunk in pd.read_csv(url, chunksize = 100, dtype = dtypes):
-        yield chunk
+
+    for index, url in enumerate(urls):
+        logging.info(f'fetching data from {url}...')
+        for chunk in pd.read_csv(url, chunksize = 100, dtype = dtypes):
+            chunk['TYPE'] = labels[index]
+            yield chunk
 
 @functions_framework.http
 def main(request):
